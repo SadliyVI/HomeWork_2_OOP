3e8cff6eaed188279b5bcdd1536fc4c0dd80c379
VIS-73 <118935704+SadliyVI@users.noreply.github.com>
2024-11-10 22:47:09 +0300

    Change type of data and added 'get_shop_list_by_dishes' function

--- homework_2_oop.py
+++ homework_2_oop.py
@@ -35,15 +35,46 @@ def get_recipe_dictionary (file_path):
                 while counter > 0:
                     ingredient = {'ingredient_name': '', 'quantity': 0,
                                   'measure': ''}
-                    ingredient['ingredient_name'] = lines_list[index].split(
-                        '|')[0].strip()
-                    ingredient['quantity'] = lines_list[index].split('|')[1].strip()
+                    ingredient['ingredient_name'] = lines_list[index].split('|')[0].strip()
+                    ingredient['quantity'] = float(lines_list[index].split(
+                        '|')[
+                        1].strip())
                     ingredient['measure'] = lines_list[index].split('|')[2].strip()
                     cook_book[line].append(ingredient)
                     counter -= 1
                     index += 1
         return cook_book
 
-import pprint
+# import pprint
+#
+# pprint.pp(get_recipe_dictionary(get_file_path('recipe_book.txt')), width = 110)
 
-pprint.pp(get_recipe_dictionary(get_file_path('recipe_book.txt')), width = 110)
+def get_shop_list_by_dishes(dishes_list, person_count):
+    """
+    Return a dictionary with the shopping list for a group of people.
+    Each key in the dictionary is a dish name, and the value is a list of dictionaries,
+    where each dictionary represents an ingredient with its quantity and measure.
+
+    Args:
+        dishes (list): List of dishes
+        person_count (int): Number of people in the group
+    """
+    cook_book = get_recipe_dictionary(get_file_path('recipe_book.txt'))
+    shopping_list = {}
+    for dish in dishes_list:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                if ingredient['ingredient_name'] not in shopping_list:
+                    shopping_list[ingredient['ingredient_name']] = {}
+                    shopping_list[ingredient['ingredient_name']] ={
+                        'measure': ingredient['measure'],
+                        'quantity': float(ingredient['quantity']) *
+                                    person_count}
+                else: shopping_list[ingredient['ingredient_name']][
+                    'quantity'] += (float(ingredient['quantity']) *
+                                    person_count)
+
+    return shopping_list
+
+print(get_shop_list_by_dishes(['Омлет', 'Запеченный картофель',
+                               'Салат Оливье'], 5))
